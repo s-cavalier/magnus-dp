d18df7e52fdc6081fef7eb0579913f4001f8a344
s-cavalier <jrc974@ucsb.edu>
2026-06-02 23:55:19 -0700

    dispatch

---
+++ src/dispatch_codegen/manual_impls.hpp
@@ -0,0 +1,8 @@
+#ifndef __MANUAL_IMPLS_HPP__
+#define __MANUAL_IMPLS_HPP__
+#include "../matrix.hpp"
+#include "../dispatcher.hpp"
+
+
+
+#endif
--- src/dispatcher.hpp
+++ src/dispatcher.hpp
@@ -5,168 +5,86 @@
 #include <concepts>
 #include <tuple>
 #include <stdexcept>
+#include <variant>
 #include "memorybuffer.hpp"
 #include "integrate.hpp"
 #include "magnus.hpp"
 
 namespace Magnus {
 
-    namespace Dispatch {
+    template <class NumT>
+    concept valid_num_t = 
+        std::same_as<NumT, float> || 
+        std::same_as<NumT, double> || 
+        std::same_as<NumT, std::complex<float>> || 
+        std::same_as<NumT, std::complex<double>>;
 
-        enum class Type : unsigned char {
-            R32,
-            R64,
-            C32,
-            C64
-        };        
+    struct Parameters {
+        template <class NumT>
+        using alloc_t = MemoryBuffer::Allocator<NumT, 8>;
+        using f32 = float;
+        using f64 = double;
+        using c32 = std::complex<float>;
+        using c64 = std::complex<double>;
 
-        struct Parameters {
-            void* in;             
-            void* out; 
-            Type dtype;
-            size_t n;
-            size_t dim;
-            size_t samples;
-            double t0; double tf;
-            MemoryBuffer::Allocator<std::byte, 8> alloc; 
-            // set to std::byte so it can be freely swapped for float/double/c32/c64/ 
+        template <valid_num_t NumT>
+        struct typed_data {
+            NumT* in;
+            NumT* out;
+            alloc_t<NumT> alloc;
         };
 
-        template <Integrator Int>
-        void type_helper_one( Parameters& param ) {
-            switch ( param.dtype ) {
-                case Type::R32: 
-                    one<Int>(
-                        typename Int::matrix_t( (float*)param.out, param.dim ),
-                        param.n,
-                        typename Int::matrix_span_t( (float*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<float, 8>(param.alloc)
-                    );
-                    break;
-                
-                case Type::R64: 
-                    one<Int>(
-                        typename Int::matrix_t( (double*)param.out, param.dim ),
-                        param.n,
-                        typename Int::matrix_span_t( (double*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<double, 8>(param.alloc)
-                    );
-                    break;
-                case Type::C32: 
-                    one<Int>(
-                        typename Int::matrix_t( (std::complex<float>*)param.out, param.dim ),
-                        param.n,
-                        typename Int::matrix_span_t( (std::complex<float>*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<std::complex<float>, 8>(param.alloc)
-                    );
-                    break;
-                case Type::C64: 
-                    one<Int>(
-                        typename Int::matrix_t( (std::complex<double>*)param.out, param.dim ),
-                        param.n,
-                        typename Int::matrix_span_t( (std::complex<double>*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<std::complex<double>, 8>(param.alloc)
-                    );
-                    break;
+        std::variant<typed_data<f32>, typed_data<f64>, typed_data<c32>, typed_data<c64>> data;
+        size_t n;
+        size_t dim;
+        size_t samples;
+        f64 t0; f64 tf;
 
-                default: throw std::runtime_error("magnus.one only supports dtypes float32, float64, complex64, complex128.");
-            }
+        template <valid_num_t NumT>
+        Parameters( NumT* in, NumT* out, const alloc_t<NumT>& alloc, size_t n, size_t dim, size_t samples, f64 t0, f64 tf ) :
+            data( std::in_place_type<typed_data<NumT>>, in, out, alloc ),
+            n(n), dim(dim),
+            samples(samples),
+            t0(t0), tf(tf)
+        {}
 
+        template <valid_num_t NumT>
+        NumT* in_data() {
+            return std::get<typed_data<NumT>>( data ).in;
         }
 
-        template <Integrator Int>
-        void type_helper_many( Parameters& param ) {
-            switch ( param.dtype ) {
-                case Type::R32: 
-                    many<Int>(
-                        typename Int::matrix_span_t( (float*)param.out, param.dim, param.n ),
-                        typename Int::matrix_span_t( (float*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<float, 8>(param.alloc)
-                    );
-                    break;
-                
-                case Type::R64: 
-                    many<Int>(
-                        typename Int::matrix_span_t( (double*)param.out, param.dim, param.n ),
-                        typename Int::matrix_span_t( (double*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<double, 8>(param.alloc)
-                    );
-                    break;
-                case Type::C32: 
-                    many<Int>(
-                        typename Int::matrix_span_t( (std::complex<float>*)param.out, param.dim, param.n ),
-                        typename Int::matrix_span_t( (std::complex<float>*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<std::complex<float>, 8>(param.alloc)
-                    );
-                    break;
-                case Type::C64: 
-                    many<Int>(
-                        typename Int::matrix_span_t( (std::complex<double>*)param.out, param.dim, param.n ),
-                        typename Int::matrix_span_t( (std::complex<double>*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<std::complex<double>, 8>(param.alloc)
-                    );
-                    break;
-
-                default: throw std::runtime_error("magnus.many only supports dtypes float32, float64, complex64, complex128.");
-            }
-
-        }
-        
-        template <Integrator Int>
-        void type_helper_sum( Parameters& param ) {
-            switch ( param.dtype ) {
-                case Type::R32: 
-                    sum<Int>(
-                        typename Int::matrix_t( (float*)param.out, param.dim ),
-                        param.n,
-                        typename Int::matrix_span_t( (float*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<float, 8>(param.alloc)
-                    );
-                    break;
-                
-                case Type::R64: 
-                    sum<Int>(
-                        typename Int::matrix_t( (double*)param.out, param.dim ),
-                        param.n,
-                        typename Int::matrix_span_t( (double*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<double, 8>(param.alloc)
-                    );
-                    break;
-                case Type::C32: 
-                    sum<Int>(
-                        typename Int::matrix_t( (std::complex<float>*)param.out, param.dim ),
-                        param.n,
-                        typename Int::matrix_span_t( (std::complex<float>*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<std::complex<float>, 8>(param.alloc)
-                    );
-                    break;
-                case Type::C64: 
-                    sum<Int>(
-                        typename Int::matrix_t( (std::complex<double>*)param.out, param.dim ),
-                        param.n,
-                        typename Int::matrix_span_t( (std::complex<double>*)param.in, param.dim, param.samples ),
-                        param.t0, param.tf,
-                        MemoryBuffer::Allocator<std::complex<double>, 8>(param.alloc)
-                    );
-                    break;
-
-                default: throw std::runtime_error("magnus.sum only supports dtypes float32, float64, complex64, complex128.");
-            }
-
+        template <valid_num_t NumT>
+        NumT* out_data() {
+            return std::get<typed_data<NumT>>( data ).out;
         }
 
-    }
+        template <valid_num_t NumT>
+        const alloc_t<NumT>& get_allocator() const {
+            return std::get<typed_data<NumT>>( data ).alloc;
+        }
+
+        template<class Visitor>
+        decltype(auto) visit(Visitor&& visitor) & {
+            return std::visit(std::forward<Visitor>(visitor), data);
+        }
+
+        template<class Visitor>
+        decltype(auto) visit(Visitor&& visitor) const& {
+            return std::visit(std::forward<Visitor>(visitor), data);
+        }
+
+        template<class Visitor>
+        decltype(auto) visit(Visitor&& visitor) && {
+            return std::visit(std::forward<Visitor>(visitor), std::move(data));
+        }
+
+        template<class Visitor>
+        decltype(auto) visit(Visitor&& visitor) const&& {
+            return std::visit(std::forward<Visitor>(visitor), std::move(data));
+        }
+
+    };
+
 
 }
 
