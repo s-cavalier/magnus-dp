af362e1295fdc2cb4f4810ac13476aa2cc0c183c
s-cavalier <jrc974@ucsb.edu>
2026-06-25 02:07:20 -0400

    update build script

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ git submodule update --init --recursive
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE -G Ninja
 (
     cd build
-    ninja -C build
 )
+ninja -C build
 (
-    cd test
+    cd demo
     python -c "import magnus; magnus.max_order(); print('Smoke test succeeded. Package should be good to go.')"
 )
